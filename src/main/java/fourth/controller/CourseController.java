26ec818bcd4748bd8efb45cc53b360b168a99a4f
eeit49 <fock360man@gmail.com>
2022-09-26 10:27:10 +0800

    宇翔

--- src/main/java/fourth/controller/CourseController.java
+++ src/main/java/fourth/controller/CourseController.java
@@ -7,12 +7,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import fourth.bean.CourseBean;
@@ -171,16 +173,18 @@ public class CourseController {
 
 	}
 
-	@GetMapping("/course.delete")
-	public String delete(int course_id) {
+//	@ResponseBody
+	@GetMapping("/course.delete/{id}")
+	public String delete(@PathVariable("id") int course_id) {
 		cService.deleteOne(course_id);
 		return "redirect:/course.list";
 	}
+	
+//	@GetMapping("/course.delete")
+//	public String delete(int course_id) {
+//		cService.deleteOne(course_id);
+//		return "redirect:/course.list";
+//	}
 
-//	@GetMapping("/delete/{id}")    
-//    public String delete(@PathVariable(value = "id") int course_id){    
-//		cService.deleteOne(course_id);    
-//        return "redirect:/courseList";    
-//    }
 
 }
