d176a4e34001633c168f9c7448e4e5a49dabde14
wayne <ggyy45529441@gmail.com>
2022-10-17 00:21:22 +0800

    1017正偉

--- src/main/java/fourth/dao/LogRepository.java
+++ src/main/java/fourth/dao/LogRepository.java
@@ -1,9 +1,11 @@
 package fourth.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fourth.bean.Log;
 
 public interface LogRepository extends JpaRepository<Log, String> {
-
+	public List<Log> findAllByOrderByOperateDateDesc();
 }
